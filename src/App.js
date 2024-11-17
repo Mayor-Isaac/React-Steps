6daa596286be09cb3de0696a65ff109019e59422
Mayor-Isaac <ogunyilekaferanmi@gmail.com>
2024-11-17 16:26:46 +0100

    17-11-eve

--- src/App.js
+++ src/App.js
@@ -1,135 +1,53 @@
-// import "./App.css";
-import "./index.css";
-// import { useState } from "react";
-
-const pizzaData = [
-  {
-    name: "Focaccia",
-    ingredients: "Bread with italian olive oil and rosemary",
-    price: 6,
-    photoName: "pizzas/focaccia.jpg",
-    soldOut: false,
-  },
-  {
-    name: "Pizza Margherita",
-    ingredients: "Tomato and mozarella",
-    price: 10,
-    photoName: "pizzas/margherita.jpg",
-    soldOut: false,
-  },
-  {
-    name: "Pizza Spinaci",
-    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
-    price: 12,
-    photoName: "pizzas/spinaci.jpg",
-    soldOut: false,
-  },
-  {
-    name: "Pizza Funghi",
-    ingredients: "Tomato, mozarella, mushrooms, and onion",
-    price: 12,
-    photoName: "pizzas/funghi.jpg",
-    soldOut: false,
-  },
-  {
-    name: "Pizza Salamino",
-    ingredients: "Tomato, mozarella, and pepperoni",
-    price: 15,
-    photoName: "pizzas/salamino.jpg",
-    soldOut: true,
-  },
-  {
-    name: "Pizza Prosciutto",
-    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
-    price: 18,
-    photoName: "pizzas/prosciutto.jpg",
-    soldOut: false,
-  },
+import { useState } from "react";
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
 ];
 
-function App() {
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+  const previousFunction = () => {
+    step === 1 ? setStep(messages.length) : setStep(step - 1);
+  };
+  const nextFunction = () => {
+    step === messages.length ? setStep(1) : setStep(step + 1);
+  };
+  const handleCard = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    // <div className="App">
-    <div className="container">
-      <Header />
-      <Menu />
-      <Footer />
-    </div>
-  );
-}
-function Header() {
-  return (
-    <header className="header footer">
-      <h1>Fast React Pizza Co.</h1>
-    </header>
-  );
-}
-function Menu() {
-  const pizzas = pizzaData;
-  const numPizzas = pizzas.length;
-  return (
-    <main className="menu">
-      <h2>Our Menu</h2>
-      {numPizzas > 0 ? (
-        <>
-          <p>
-            Authentic Italian cuisine, {numPizzas} creative dishes to choose
-            from. All from our stone oven, all organic, all delicious.
+    <>
+      <button className="close" onClick={handleCard}>
+        {isOpen ? "x" : "="}
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
           </p>
-          <ul className="pizzas">
-            {pizzas.map((pizza) => (
-              <Pizza
-                name={pizza.name}
-                ingredients={pizza.ingredients}
-                photoName={pizza.photoName}
-                price={pizza.price}
-                soldOut={pizza.soldOut}
-                key={pizza.name}
-              />
-            ))}
-          </ul>
-        </>
-      ) : (
-        <p>Sorry, we are updating our menu</p>
+          <div className="buttons">
+            <button
+              onClick={previousFunction}
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            >
+              Previous
+            </button>
+            <button
+              onClick={nextFunction}
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            >
+              Next
+            </button>
+          </div>
+        </div>
       )}
-    </main>
+    </>
   );
 }
-
-function Pizza(props) {
-  // console.log(props);
-  // if (props.soldOut) return null;
-  return (
-    <li className={`pizza ${props.soldOut && "sold-out"}`}>
-      <img src={props.photoName} alt={props.name} />
-      <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.soldOut ? "SOLD OUT" : props.price}</span>
-      </div>
-    </li>
-  );
-}
-function Footer() {
-  const hour = new Date().getHours();
-  const openHour = 10;
-  const closeHour = 22;
-  const isOpen = hour >= openHour && hour <= closeHour;
-  return (
-    <footer className="footer">
-      {isOpen ? (
-        <Order closeHour={closeHour} />
-      ) : (
-        <p>Sorry, we are yet to open</p>
-      )}
-    </footer>
-  );
-}
-function Order(props) {
-  <div className="order">
-    <p>We're open until {props.closeHour}:00. Come visit us or order online.</p>
-    <button className="btn">Order</button>
-  </div>;
-}
-
-export default App;
